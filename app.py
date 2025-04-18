cbc0aa40d9824ae4bccde90a63cb1cb8b02fdede
SamarthKharade <kharadesamarth64@gmail.com>
2025-04-19 02:45:40 +0530

    Fix dynamic port handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os  # To use the environment variable for port
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def submit_form():
     return 'Thank you for your message!'
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or fallback to 5000
+    app.run(host="0.0.0.0", port=port)
